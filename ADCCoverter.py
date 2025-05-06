141a376b1e16795eaec2f5700e30747ac6c6e7af
Thatotheron32 <157820420+Thatotheron32@users.noreply.github.com>
2025-05-06 11:50:34 -0500

    Changed resistance numbers to on board numbers

--- ADCCoverter.py
+++ ADCCoverter.py
@@ -2,10 +2,10 @@ def ADCTOV(VADC, V_ref):
     """Calculates the relative Voltage from the ADC"""
     ADCVoltage = (VADC * 4096) * V_ref
     return ADCVoltage
-    
+
 def VDIVADC(VinputADC):
     """Calcuateds the real Voltage at the Voltage div"""
-    realVoltage = (VinputADC * 21100)/1100
+    realVoltage = (VinputADC * 21100)/1000
     return realVoltage 
 
 def ASensorADC(CinputADC):
